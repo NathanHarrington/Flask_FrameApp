8cfbd6443a94d59564b18843f7d3ac11c4417c17
Nathan Harrington <harrington.nathan@gmail.com>
2019-04-13 07:52:59 -0400

    flask bootstrap removal, locally served bootstrap files

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
 
@@ -20,7 +19,6 @@ login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = 'Please log in to access this page.'
 mail = Mail()
-bootstrap = Bootstrap()
 moment = Moment()
 
 
@@ -32,7 +30,6 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
-    bootstrap.init_app(app)
     moment.init_app(app)
 
     # Register blueprints
